8fa7a5924863f3df62cb6bab515b11b35bbc4d42
bli <bla@blubb.com>
2020-03-23 02:02:17 +0100

    init

---
+++ src/main/java/de/joern/playground/simple_spring/configuration/MyConfiguration.java
@@ -0,0 +1,25 @@
+package de.joern.playground.simple_spring.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "de.joern.playground.simple_spring")
+public class MyConfiguration {
+	
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+ 
+}
